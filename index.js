624e8a48fb240758dc16160d483e0695be74573c
Ryan Motchkavitz <ryanmotch2@gmail.com>
2026-03-08 10:47:28 -0400

    js

---
+++ index.js
@@ -0,0 +1,8 @@
+let code1;
+code1 = 7 + 3;
+let code2 = (82/2)-1;
+let code3 = 79 % 40;
+let message = "The vault has been secured. The combination is:";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB )
